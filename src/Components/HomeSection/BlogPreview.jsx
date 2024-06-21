25bd1954ca97cff354e44f5bd0f0d836f664f920
David-Avila <aviladurandavid613@gmail.com>
2024-06-21 15:52:05 -0400

    Blogs preview and saving to database

--- src/Components/HomeSection/BlogPreview.jsx
+++ src/Components/HomeSection/BlogPreview.jsx
@@ -1,9 +1,15 @@
+import '../../App.css'
 
 export function BlogPreview({data}){
+
+    function openBlog() {
+
+    }
+
     return (
-        <section className="blogPreview">
+        <section onClick={openBlog} className="blogPreview no-select">
             <h2>{data.title}</h2>
-            <h3>{data.author}</h3>
+            <h3>By: {data.author}</h3>
         </section>
     )
 }
--- src/Components/HomeSection/BlogsGrid.jsx
+++ src/Components/HomeSection/BlogsGrid.jsx
@@ -1,11 +1,104 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { BlogPreview } from "./BlogPreview";
+import sb from '../../../Private/SupabaseClient'
+import {v4 as uuidv4} from 'uuid'
+import { ContentContext } from "../ContentProvider";
 
 export function BlogsGrid(){
     const [blogs, setBlogs] = useState();
+    const content = useContext(ContentContext);
+
+    useEffect(() => {
+        loadBlogs()
+        .then(res => {
+            setBlogs(res);
+        })
+    }, [])
 
     async function loadBlogs(){
-        
+        const { data, error } = await sb
+            .from("Blogs")
+            .select("*")
+
+        if (error){
+            alert(error.message);
+        }        
+
+        if (data){
+            return data;
+        }
     }
 
-    return <h3>Blogs Grid</h3>
+    function addBlog(){
+        if (!content.currentUser){
+            alert("You have to be loged in to create a blog");
+            return
+        }
+
+        const newBlog = {
+            title: "Test Blog",
+            description: "Blog added for testing",
+            blog_id: uuidv4(),
+            author: content.currentUser.username,
+        }
+
+        // Check if blog title already exists
+        const checkTitle = async () => {
+            const { data, error} = await sb
+                .from("Blogs")
+                .select("*")
+                .eq("title", newBlog.title)
+
+            if (error){
+                alert(error);
+            }
+
+            if (data){
+                return data;
+            }
+        }
+
+        checkTitle()
+        .then(res => {
+            if (res == undefined || res.length === 0){
+                //Add new blog
+
+                const add = async () => {
+                    const {data, error} = await sb
+                        .from("Blogs")
+                        .insert({
+                            title: newBlog.title,
+                            description: newBlog.description,
+                            blog_id: newBlog.blog_id,
+                            author: newBlog.author,
+                        })
+
+                    if (error){
+                        alert(error);
+                    }
+                }
+
+                add();
+
+            }
+            else {
+                alert("Blog already exists");
+            }
+        })
+
+    }
+
+    return (
+        <div className="blogsGrid flex">
+            {(blogs != undefined && blogs.length > 0)
+            && blogs.map(blog => {
+                return <BlogPreview data={blog}/>
+            })}
+
+            <div onClick={addBlog} className="blogPreview flex no-select">
+                <h2>Create your blog</h2>
+            </div>
+
+        </div>
+    )
 }
