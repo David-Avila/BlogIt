7cffe81487ad71b9bfe6ec559898ce2bd8240462
David-Avila <aviladurandavid613@gmail.com>
2024-06-24 07:49:53 -0400

    Fill up contact section

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -1,7 +1,44 @@
 import '../../App.css'
+import { NavBarItem } from '../NavBar/NavBarItem'
+import { FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa6'
 
 export function ContactSection(){
+    const iconSize = 32;
+
+    function goTo(link){
+        window.open(link, '_blank');
+    }
+
     return (
-        <h2>Contact Section</h2>
+        <section className='flex column padding'>
+            <h3>Hi, thanks for using BlogIt</h3>
+            <p>
+                If you have a bug or want a specific feature, contact me with the 
+                following links.
+            </p>
+            <div className='flex row socialIcons'>
+                <NavBarItem data={{
+                        action: () => {goTo("www.linkedin.com/in/david-avila-duran-12568a222")}, 
+                        icon: <FaLinkedin size={iconSize}/>,
+                        tooltip: "LinkedIn",
+                        }} 
+                />
+                <NavBarItem data={{
+                        action: () => {goTo("https://github.com/David-Avila")}, 
+                        icon: <FaGithub size={iconSize}/>,
+                        tooltip: "GitHub",
+                        }} 
+                />
+                
+                
+            </div>
+        </section>
     )
 }
+/* GMAIL
+<NavBarItem data={{
+                        action: () => {}, 
+                        icon: <FaGoogle size={iconSize}/>,
+                        tooltip: "Gmail",
+                        }} 
+                />*/
